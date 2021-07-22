8fe9aa451dcac1cf8b36352516f84b46815dcec2
tristkick1234 <40550566+Tristkick1234@users.noreply.github.com>
2021-07-22 20:34:42 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Embroupler la petiete  ");
+      Console.Write("beepepeboo bangaoala on the same line ");
+      Console.Write("iakjsla "); 
+      //comment
+    }
+  }
+}
