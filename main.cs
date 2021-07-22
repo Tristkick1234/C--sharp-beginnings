6223fd0694d5b96f2ad57aa98ae9e3214d616a46
AFRICAN BOI <40550566+Tristkick1234@users.noreply.github.com>
2021-07-22 22:03:26 +0000

    Added an intro

--- main.cs
+++ main.cs
@@ -6,10 +6,7 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Embroupler la petiete  ");
-      Console.Write("beepepeboo bangaoala on the same line ");
-      Console.Write("iakjsla "); 
-      //comment
+      Console.WriteLine("Welcome to my C# experience! ");
     }
   }
 }
