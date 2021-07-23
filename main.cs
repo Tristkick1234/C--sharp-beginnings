a25cdee63fe921bd79d3c67afed45c3118577cd3
AFRICAN BOI <40550566+Tristkick1234@users.noreply.github.com>
2021-07-23 21:49:19 +0000

    Eeverything

--- main.cs
+++ main.cs
@@ -6,7 +6,29 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to my C# experience! ");
+      string[] money = {"Peso", "Kwanzaa", "Dinar"};
+      Array.Sort(money);
+      Console.WriteLine(money.Length);
+
+      for (int i = 0; i < money.Length; i++)
+      {
+        Console.WriteLine(money[i]);
+      }
+
+      foreach (string i in money)
+      {
+        Console.WriteLine(i);
+      }
+
+      string[] dogs;
+
+      dogs = new string[]  {" german", " doge"};
+
+      foreach (string i in dogs)
+      {
+        Console.WriteLine(i);
+      }
+      
     }
   }
 }
