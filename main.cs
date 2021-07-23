f1987bb4290bec5624543fa962abca1acb9070dd
AFRICAN BOI <40550566+Tristkick1234@users.noreply.github.com>
2021-07-23 21:16:07 +0000

    Added Looops

--- main.cs
+++ main.cs
@@ -6,7 +6,34 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to my C# experience! ");
+      //loops
+      int i = 0;
+      while (i < 9)
+      {
+        Console.WriteLine(i);
+        i++;
+      }
+      int b = 0;
+      do
+      {
+        Console.WriteLine(b);
+        b++;
+      }
+      while(b < 9);
+
+      //for loops
+      for(int g = 0; g < 10; g++)
+      {
+        Console.WriteLine($"This is a {g}");
+      }
+
+      string[] progrmLang = {"C#","JavaScript", "C++", "Python"};
+      foreach(string x in progrmLang)
+      {
+        Console.WriteLine(x + " Is one of the languages.");
+      }
+
+      //Break and COntinue
     }
   }
 }
