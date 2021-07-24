268611c9b2cf6d8ffa07b6a17b39a5dc56e962da
AFRICAN BOI <40550566+Tristkick1234@users.noreply.github.com>
2021-07-24 19:12:39 +0000

    Added Nested if statements and quite a few more things

--- main.cs
+++ main.cs
@@ -6,22 +6,117 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the simple calculator. Would you like to add, multiply, subtract, divide, exponent, or exit? ");
+      Console.WriteLine("Welcome to the simple calculator. Would you like to add, multiply, subtract, divide, or exit? ");
 
-      string response = Convert.ToInt32(Console.ReadLine());
+      string response = Console.ReadLine();
       //if exit then break loop
 
-      int x;
-      int y;
-      int i = 0;
-      while (i < 5)
+   
+      
+      while (true)
       {
         if (response == "add")
-        Console.WriteLine("Choose two numbers");
+        {
+          Console.WriteLine("Choose two numbers");
 
-        int Adding = Console.Readline(x + y) ;
-        i++;
+          int x = Convert.ToInt32(Console.ReadLine());
+          int y = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine(x + y);
+          string jac = Console.ReadLine();
+
+          if (x == 0 && y == 0);
+          {
+            Console.WriteLine("Continue or Exit ");
+            string decide = Console.ReadLine();
+            if (decide == "continue")
+            {
+              continue;
+            }
+            else
+            {
+              break;
+            }
+          } 
+        }
+        else if (response == "subtract" || jac == "subtract")
+        {
+          Console.WriteLine("Choose two numbers");
+
+          int x = Convert.ToInt32(Console.ReadLine());
+          int y = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine(x - y);
+
+          if (x == 0 & y == 0);
+          {
+            Console.WriteLine("Continue or Exit ");
+            string decide = Console.ReadLine();
+            if (decide == "continue")
+            {
+              continue;
+            }
+            else
+            {
+              break;
+            }
+          } 
+        }
+        else if (response == "multiply")
+        {
+          Console.WriteLine("Choose two numbers");
+
+          int x = Convert.ToInt32(Console.ReadLine());
+          int y = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine(x * y);
+
+          if (x == 0 & y == 0);
+          {
+            Console.WriteLine("Continue or Exit ");
+            string decide = Console.ReadLine();
+            if (decide == "continue")
+            {
+              continue;
+            }
+            else
+            {
+              break;
+            }
+          } 
+        }
+        else if (response == "divide")
+        {
+          Console.WriteLine("Choose two numbers");
+
+          double x = Convert.ToDouble(Console.ReadLine());
+          double y = Convert.ToDouble(Console.ReadLine());
+          Console.WriteLine(x / y);
+
+          if (x == 0 & y == 0);
+          {
+            Console.WriteLine("Continue or Exit ");
+            string decide = Console.ReadLine();
+            if (decide == "continue")
+            {
+              continue;
+            }
+            else
+            {
+              break;
+            }
+          } 
+        }
+
+        else if (response == "exit")
+        {
+          break;
+        }
+
+        else
+        {
+          break;
+        }
+
       }
+      Console.WriteLine("Calculator closed.");
     }
   }
 }
