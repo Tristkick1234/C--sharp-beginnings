7dc68d7187e1e183fe7e04e8d9472093bb9e64f8
AFRICAN BOI <40550566+Tristkick1234@users.noreply.github.com>
2021-07-24 18:10:06 +0000

    Added loop and variables

--- main.cs
+++ main.cs
@@ -6,7 +6,22 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to my C# experience! ");
+      Console.WriteLine("Welcome to the simple calculator. Would you like to add, multiply, subtract, divide, exponent, or exit? ");
+
+      string response = Convert.ToInt32(Console.ReadLine());
+      //if exit then break loop
+
+      int x;
+      int y;
+      int i = 0;
+      while (i < 5)
+      {
+        if (response == "add")
+        Console.WriteLine("Choose two numbers");
+
+        int Adding = Console.Readline(x + y) ;
+        i++;
+      }
     }
   }
 }
