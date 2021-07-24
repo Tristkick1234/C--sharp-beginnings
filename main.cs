8f4c435d96233346113266186ec1670e14fd206e
AFRICAN BOI <40550566+Tristkick1234@users.noreply.github.com>
2021-07-24 18:11:33 +0000

    a

--- main.cs
+++ main.cs
@@ -6,7 +6,29 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to my C# experience! ");
+     /*methods can call also known as functions */
+
+     for (int i = 0; i < 5; i++)
+     {
+       MyMethod(3, 2);
+     };
+
+     MethodTwo(a:1, b:2, c:3);
+     
+
+
+
+    }
+
+    static void MethodTwo(int a, int b, int c)
+    {
+      Console.WriteLine("It is " + c);
+    }
+
+    static void MyMethod(int x, int y)
+    {
+      
+      Console.WriteLine(x + y);
     }
   }
 }
